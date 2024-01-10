a04e38ca26894fa00d1884853048fbb917941fb0
alanpaterson82 <>
2024-01-10 20:25:28 +0000

    end of tutorial

--- sql-crud.py
+++ sql-crud.py
@@ -119,24 +119,24 @@ alan_paterson = Programmer(
 
 
 # deleting a single record
-fname = input("Enter a first name: ")
-lname = input("Enter a last name: ")
-programmer = session.query(Programmer).filter_by(first_name=fname,
-                                                 last_name=lname).first()
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by
+# (first_name=fname, last_name=lname).first()
 # defensive programming
 # if programmer is not None:
-#    print("Programmer Found: ", programmer.first_name + " " +
-          programmer.last_name)
-    confirmation = input("Are you sure you want to delete this record?
-                         (y/n) ")
-    if confirmation.lower() == "y":
-        session.delete(programmer)
-        session.commit()
-    print("Programmer has been deleted")
-    else:
-    print("Programmer not deleted")
-else:
-    print("No records found")
+#     print
+# ("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input
+# ("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
 
 
 # delete multiple/all records
